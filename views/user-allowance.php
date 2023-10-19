82a7a339237d22d801bfb5e6bc71d371a0dca9fe
jmLecias <104704307+jmLecias@users.noreply.github.com>
2023-10-20 00:08:07 +0800

    admin-userlist page connects to user-allowance

--- views/user-allowance.php
+++ views/user-allowance.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../db_conn.php');
-include 'user-allowance-class.php';
-    ?>
+include 'entity-classes.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,6 +13,86 @@ include 'user-allowance-class.php';
     <title>Allowance Page</title>
 </head>
 
+<style>
+    .list-div {
+        width: 67%;
+        min-height: 400px;
+        float: left;
+    }
+
+    .list-header-div {
+        display: flex;
+        width: 100%;
+        min-height: 60px;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        margin-top: 10px;
+        color: #B5E3FF;
+    }
+
+    .list-body-div {
+        width: 100%;
+        color: #B5E3FF;
+    }
+
+    .listtile-div {
+        justify-content: space-between;
+        width: 100%;
+        min-height: 55px;
+        background-color: #124361;
+        margin-bottom: 6px;
+        color: #B5E3FF;
+        border-radius: 10px;
+    }
+
+    .listtile-div:hover {
+        outline: 2px #fdac5b solid;
+        color: #F99B3C;
+    }
+
+    .expenses-count-div {
+        margin-bottom: 5px;
+        border-radius: 3px;
+        border: 1px solid;
+        padding: 3px 5px;
+        font-size: 10px;
+        margin-left: 20px
+    }
+
+    .info-div {
+        width: 30%;
+        min-height: 400px;
+        float: right;
+        color: #B5E3FF;
+    }
+</style>
+
+<?php
+    $currentUser;
+    if (isset($_GET['id'])) {
+        $userID = $_GET['id'];
+        $sql = "SELECT * FROM users WHERE `userID` ='$userID'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while ($r = $result->fetch_assoc()) {
+                $currentUser = new User(
+                    $r['userID'],
+                    $r['firstname'],
+                    $r['lastname'],
+                    $r['email'],
+                    $r['role'],
+                );
+            }
+        } else {
+            header('location:admin-userlist.php');
+        }
+    } else {
+        header('location:admin-userlist.php');
+    }
+?>
+
 <body>
     <!-- Header / Logo Div -->
     <div class="header-div">
@@ -22,14 +102,14 @@ include 'user-allowance-class.php';
             <h1 class="logo-text">Money minder</h1>
         </div>
         <div class="row-div" style="cursor: pointer">
-            <img stye="position: absolute" src="../public/images/icon-account-logout.png">
+            <img stye="padding-right: 20px" src="../public/images/icon-user.png">
         </div>
     </div>
 
     <!-- Content Area -->
     <div class="body-div">
         <div class="body-top-div primary-text">
-            Hell0W4rld
+            <?php echo $currentUser->firstname.' '.$currentUser->lastname;?>
         </div>
         <div class="list-div">
             <div class="list-header-div">
@@ -45,44 +125,45 @@ include 'user-allowance-class.php';
             </div>
             <div class="list-body-div">
                 <?php
-                    $sql = "SELECT * FROM allowances";
-                    $result = $conn->query($sql);
-                    
-                    if ($result->num_rows > 0) {
-                        while ($r = $result->fetch_assoc()) {
-                            $newAllowance = new Allowance(
-                                $r['allowanceID'],
-                                $r['userID'],
-                                $r['amount'],
-                                $r['title'],
-                                $r['description'],
-                                $r['date'],
-                                $r['category']
-                            );
+                $sql = "SELECT * FROM allowances WHERE `userID` ='$userID'";
+                $result = $conn->query($sql);
 
-                            $sql = "SELECT * FROM expenses WHERE `allowanceID` = '$newAllowance->allowanceID'";
-                            $expensesCount = $conn->query($sql)->num_rows;
+                if ($result->num_rows > 0) {
+                    while ($r = $result->fetch_assoc()) {
+                        $newAllowance = new Allowance(
+                            $r['allowanceID'],
+                            $r['userID'],
+                            $r['amount'],
+                            $r['title'],
+                            $r['description'],
+                            $r['date'],
+                            $r['category']
+                        );
 
-                            echo displayAllowance($newAllowance, $expensesCount, false);
-                        }
-                    } else {
-                        echo "No Results";
+                        $sql = "SELECT * FROM expenses WHERE `allowanceID` = '$newAllowance->allowanceID'";
+                        $expensesCount = $conn->query($sql)->num_rows;
+
+                        // List Tile Div
+                        echo "
+                            <div class=\"row-div listtile-div hover-trigger\" style=\"cursor: pointer\">
+                                <div class=\"row-div\" style=\"width: 50%; justify-content: start\">
+                                    <h1 class=\"secondary-text\" style=\"font-size: 16px; margin-left: 20px\">$newAllowance->title</h1>
+                                    <div class=\"expenses-count-div\">$expensesCount expenses</div>
+                                </div>
+                                <div class=\"row-div\"\ style=\"width: 30%\; justify-content: end\">
+                                    <h1 class=\"secondary-text\" style=\"font-size: 15px;  margin-right: 20px\">PHP $newAllowance->amount</h1>
+                                    <h1 class=\"secondary-text\" style=\"font-size: 12px;  margin-right: 20px\">$newAllowance->category</h1>
+                                    <div class=\"expenses-info\" style=\"display:none;\">
+                                        <button style=\"border-radius: 13px; font-size:12px; padding: 6px 10px; margin-right: 20px\">Expenses info</button>
+                                    </div>
+                                </div>
+                            </div>
+                        ";
                     }
-                    $conn->close();
-
-                    // DESIGN SAMPLES
-                    // $allowances_sample = array(
-                    //     new Allowance(1, 2, 150.00, "Food allowance", "This is only for food", "nd", "per day"),
-                    //     new Allowance(1, 2, 5000.55, "House rent allowance", "This is only for food", "nd", "per month"),
-                    //     new Allowance(1, 2, 150.00, "Food allowance", "This is only for food", "nd", "per day"),
-                    //     new Allowance(1, 2, 150.00, "Food allowance", "This is only for food", "nd", "per day"),
-                    //     new Allowance(1, 2, 1500.45, "Food allowance", "This is only for food", "nd", "per day"),
-                    //     new Allowance(1, 2, 150.00, "Food allowance", "This is only for food", "nd", "per day"),
-                    // );
-
-                    // foreach ($allowances_sample as $allowance) {
-                    //     echo displayAllowance($allowance, 5, false);
-                    // }
+                } else {
+                    echo "No Results";
+                }
+                $conn->close();
                 ?>
             </div>
         </div>
